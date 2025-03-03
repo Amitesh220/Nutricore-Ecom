a9b6c39b813a322fc8ff287d331e7fceb493dfd0
Amitesh Mishra <94803046+Amitesh220@users.noreply.github.com>
2025-03-03 20:41:47 +0530

    Update App.jsx

--- supplement-web/src/App.jsx
+++ supplement-web/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './Components/Navbar';
 import Home from './pages/Home';
 import Categories from './pages/Categories';
 import About from './pages/About';
